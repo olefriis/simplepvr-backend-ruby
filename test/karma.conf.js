f478e0a1cf7694b395a879489941889365330978
Ole Friis <olefriis@gmail.com>
2013-12-31 13:30:34 +0100

    Using various CDNs for other JS libraries as well, drastically decreasing the size of this gem.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,12 +3,13 @@ basePath = '../';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'public/js/jquery/jquery.min.js',
+  'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular-route.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular-resource.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular-cookies.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular-mocks.js',
+  'http://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js',
   'public/js/**/*.js',
   'test/unit/**/*.js'
 ];
