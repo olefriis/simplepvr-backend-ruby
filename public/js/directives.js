9c11d058742523630e133da1124e63e5c27e2288
Ole Friis <olefriis@gmail.com>
2014-01-01 14:04:39 +0100

    Created directives.js, for all AngularJS directives in the app.

---
+++ public/js/directives.js
@@ -0,0 +1,93 @@
+'use strict';
+
+angular.module('simplePvrDirectives', []).
+directive('loginDialog', function($timeout) {
+   return {
+       templateUrl: '/app/templates/loginDialog.html',
+       restrict: 'E',
+       replace: true,
+       controller: CredentialsController,
+       link: function(scope, element, attributes, controller) {
+           var isShowing = false;
+           
+           element.on('shown.bs.modal', function(e) {
+               element.find('#userName').focus();
+           });
+
+           scope.$on('event:auth-loginRequired', function() {
+               if (isShowing) {
+                   return;
+               }
+               
+               // If we're in the process of hiding the modal, we need to wait for
+               // all CSS animations to complete before showing the modal again.
+               // Otherwise, we might end up with an invisible modal, making the whole
+               // view rather unusable. I've been unable to control the transitions
+               // between "showing", "shown", "hiding", and "hidden" tightly using
+               // JQuery notifications without collecting more and more modal backdrops
+               // in the DOM, so the dirty solution here is to simply wait a second
+               // before showing the log-in dialog.
+               isShowing = true;
+               $timeout(function() {
+                   element.modal('show');
+                   isShowing = false;
+               }, 1000);
+           });
+
+           scope.$on('event:auth-loginConfirmed', function() {
+               element.modal('hide');
+               scope.credentials.password = '';
+           });
+       }
+   } 
+}).
+directive('titleSearch', function($cookieStore) {
+    return {
+        templateUrl: '/app/templates/titleSearch.html',
+        restrict: 'E',
+		replace: true,
+		controller: SearchProgrammesCtrl,
+		link: function(scope, element, attributes, controller) {
+			var inputField = element.find('input');
+			inputField.typeahead({
+				remote: {
+		            url: '/api/programmes/title_search?query=%QUERY',
+                    beforeSend: function(jqXhr, settings) {
+                        var credentials = $cookieStore.get('basicCredentials');
+                        if (credentials) {
+                            jqXhr.setRequestHeader('Authorization', 'Basic ' + credentials);
+                        }
+                    }
+				}
+			});
+			var updateTitle = function() {
+				scope.$apply(function() {
+					scope.title = inputField.val();
+				});
+			};
+			inputField.change(updateTitle);
+			inputField.on('typeahead:autocompleted', updateTitle);
+		}
+	};
+}).
+directive('navbarItem', function($location) {
+	return {
+		template: '<li><a ng-href="{{route}}" ng-transclude></a></li>',
+		restrict: 'E',
+		transclude: true,
+		replace: true,
+		scope: { route:'@route' },
+		link: function(scope, element, attributes, controller) {
+			scope.$on('$routeChangeSuccess', function() {
+				var path = $location.path();
+				var isSamePath = path == scope.route;
+				var isSubpath = path.indexOf(scope.route + '/') == 0;
+				if (isSamePath || isSubpath) {
+					element.addClass('active');
+				} else {
+					element.removeClass('active');
+				}
+			});
+		}
+	};
+});
